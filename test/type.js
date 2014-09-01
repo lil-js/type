d5a60a276954d721167f7f22c198c33cebe61a0f
Tomas Aparicio <tomas@aparicio.me>
2014-09-01 07:28:28 +0200

    feat: add UMD support

--- test/type.js
+++ test/type.js
@@ -1,7 +1,7 @@
 describe('type', function () {
 
   it('should expose the http constructor', function () {
-    expect(lil.type).to.be.a('function')
+    expect(lil.type).to.be.an('object')
   })
 
   it('should expose the VERSION property', function () {
--- type.js
+++ type.js
@@ -1,28 +1,35 @@
 /*! lil-type - v0.1 - MIT License - https://github.com/lil-js/type */
-(function (global) {
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(['exports'], factory)
+  } else if (typeof exports === 'object') {
+    factory(exports)
+    if (typeof module === 'object' && module !== null) {
+      module.exports = exports
+    }
+  } else {
+    factory((root.lil = root.lil || {}))
+  }
+}(this, function (exports) {
   var VERSION = '0.1.0'
-  var lil = global.lil || {}
   var toStr = Object.prototype.toString
   var slice = Array.prototype.slice
 
-  function isObject(o) {
+  exports.type = { VERSION: VERSION }
+
+  exports.isObject = function isObject(o) {
     return o && toStr.call(o) === '[object Object]'
   }
 
-  function isArray(o) {
+  exports.isArray = function isArray(o) {
     return o && toStr.call(o) === '[object Array]'
   }
 
-  function type(config, data, cb) {
-    return requestFactory('GET').apply(null, arguments)
+  exports.isFn = function isFn(fn) {
+    return typeof fn === 'function'
   }
 
-  type.VERSION = VERSION
-
-  lil.type = type
-  if (typeof define === 'function' && define.amd) {
-    define(function () { return type })
-  } else {
-    global.lil = lil
+  exports.isDate = function isDate(o) {
+    return o && toStr.call(o) === '[object Date]'
   }
-}(this))
+}))
